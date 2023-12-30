e224621c18f397a09f3f2ca000633d21d6a4c735
vivekkuruppath-zaigo <vivek@zaigoinfotech.com>
2023-12-31 00:19:38 +0530

    Flutter Colors  🔥🔴🟢🔵⚪⚫🔥

---
+++ bin/colors.dart
@@ -0,0 +1,254 @@
+import 'package:flutter/material.dart';
+
+// Black Shades
+const Color black50 = Color(0xFFFAFAFA);
+const Color black100 = Color(0xFFF5F5F5);
+const Color black200 = Color(0xFFEEEEEE);
+const Color black300 = Color(0xFFE0E0E0);
+const Color black400 = Color(0xFFBDBDBD);
+const Color black500 = Color(0xFF9E9E9E);
+const Color black600 = Color(0xFF757575);
+const Color black700 = Color(0xFF616161);
+const Color black800 = Color(0xFF424242);
+const Color black900 = Color(0xFF212121);
+
+// Red Shades
+const Color red50 = Color(0xFFFFEBEE);
+const Color red100 = Color(0xFFFFCDD2);
+const Color red200 = Color(0xFFEF9A9A);
+const Color red300 = Color(0xFFE57373);
+const Color red400 = Color(0xFFEF5350);
+const Color red500 = Color(0xFFF44336);
+const Color red600 = Color(0xFFE53935);
+const Color red700 = Color(0xFFD32F2F);
+const Color red800 = Color(0xFFC62828);
+const Color red900 = Color(0xFFB71C1C);
+
+// Pink Shades
+const Color pink50 = Color(0xFFFCE4EC);
+const Color pink100 = Color(0xFFF8BBD0);
+const Color pink200 = Color(0xFFF48FB1);
+const Color pink300 = Color(0xFFF06292);
+const Color pink400 = Color(0xFFEC407A);
+const Color pink500 = Color(0xFFE91E63);
+const Color pink600 = Color(0xFFD81B60);
+const Color pink700 = Color(0xFFC2185B);
+const Color pink800 = Color(0xFFAD1457);
+const Color pink900 = Color(0xFF880E4F);
+
+// Purple Shades
+const Color purple50 = Color(0xFFF3E5F5);
+const Color purple100 = Color(0xFFE1BEE7);
+const Color purple200 = Color(0xFFCE93D8);
+const Color purple300 = Color(0xFFBA68C8);
+const Color purple400 = Color(0xFFAB47BC);
+const Color purple500 = Color(0xFF9C27B0);
+const Color purple600 = Color(0xFF8E24AA);
+const Color purple700 = Color(0xFF7B1FA2);
+const Color purple800 = Color(0xFF6A1B9A);
+const Color purple900 = Color(0xFF4A148C);
+
+// Deep Purple Shades
+const Color deepPurple50 = Color(0xFFEDE7F6);
+const Color deepPurple100 = Color(0xFFD1C4E9);
+const Color deepPurple200 = Color(0xFFB39DDB);
+const Color deepPurple300 = Color(0xFF9575CD);
+const Color deepPurple400 = Color(0xFF7E57C2);
+const Color deepPurple500 = Color(0xFF673AB7);
+const Color deepPurple600 = Color(0xFF5E35B1);
+const Color deepPurple700 = Color(0xFF512DA8);
+const Color deepPurple800 = Color(0xFF4527A0);
+const Color deepPurple900 = Color(0xFF311B92);
+
+// Indigo Shades
+const Color indigo50 = Color(0xFFE8EAF6);
+const Color indigo100 = Color(0xFFC5CAE9);
+const Color indigo200 = Color(0xFF9FA8DA);
+const Color indigo300 = Color(0xFF7986CB);
+const Color indigo400 = Color(0xFF5C6BC0);
+const Color indigo500 = Color(0xFF3F51B5);
+const Color indigo600 = Color(0xFF3949AB);
+const Color indigo700 = Color(0xFF303F9F);
+const Color indigo800 = Color(0xFF283593);
+const Color indigo900 = Color(0xFF1A237E);
+
+// Blue Shades
+const Color blue50 = Color(0xFFE3F2FD);
+const Color blue100 = Color(0xFFBBDEFB);
+const Color blue200 = Color(0xFF90CAF9);
+const Color blue300 = Color(0xFF64B5F6);
+const Color blue400 = Color(0xFF42A5F5);
+const Color blue500 = Color(0xFF2196F3);
+const Color blue600 = Color(0xFF1E88E5);
+const Color blue700 = Color(0xFF1976D2);
+const Color blue800 = Color(0xFF1565C0);
+const Color blue900 = Color(0xFF0D47A1);
+
+// Light Blue Shades
+const Color lightBlue50 = Color(0xFFE1F5FE);
+const Color lightBlue100 = Color(0xFFB3E0F2);
+const Color lightBlue200 = Color(0xFF81D4FA);
+const Color lightBlue300 = Color(0xFF4FC3F7);
+const Color lightBlue400 = Color(0xFF29B6F6);
+const Color lightBlue500 = Color(0xFF03A9F4);
+const Color lightBlue600 = Color(0xFF039BE5);
+const Color lightBlue700 = Color(0xFF0288D1);
+const Color lightBlue800 = Color(0xFF0277BD);
+const Color lightBlue900 = Color(0xFF01579B);
+
+// Cyan Shades
+const Color cyan50 = Color(0xFFE0F7FA);
+const Color cyan100 = Color(0xFFB2EBF2);
+const Color cyan200 = Color(0xFF80DEEA);
+const Color cyan300 = Color(0xFF4DD0E1);
+const Color cyan400 = Color(0xFF26C6DA);
+const Color cyan500 = Color(0xFF00BCD4);
+const Color cyan600 = Color(0xFF00ACC1);
+const Color cyan700 = Color(0xFF0097A7);
+const Color cyan800 = Color(0xFF00838F);
+const Color cyan900 = Color(0xFF006064);
+
+// Teal Shades
+const Color teal50 = Color(0xFFE0F2F1);
+const Color teal100 = Color(0xFFB2DFDB);
+const Color teal200 = Color(0xFF80CBC4);
+const Color teal300 = Color(0xFF4DB6AC);
+const Color teal400 = Color(0xFF26A69A);
+const Color teal500 = Color(0xFF009688);
+const Color teal600 = Color(0xFF00897B);
+const Color teal700 = Color(0xFF00796B);
+const Color teal800 = Color(0xFF00695C);
+const Color teal900 = Color(0xFF004D40);
+
+// Green Shades
+const Color green50 = Color(0xFFE8F5E9);
+const Color green100 = Color(0xFFC8E6C9);
+const Color green200 = Color(0xFFA5D6A7);
+const Color green300 = Color(0xFF81C784);
+const Color green400 = Color(0xFF66BB6A);
+const Color green500 = Color(0xFF4CAF50);
+const Color green600 = Color(0xFF43A047);
+const Color green700 = Color(0xFF388E3C);
+const Color green800 = Color(0xFF2E7D32);
+const Color green900 = Color(0xFF1B5E20);
+
+// Light Green Shades
+const Color lightGreen50 = Color(0xFFF1F8E9);
+const Color lightGreen100 = Color(0xFFDCEDC8);
+const Color lightGreen200 = Color(0xFFC5E1A5);
+const Color lightGreen300 = Color(0xFFAED581);
+const Color lightGreen400 = Color(0xFF9CCC65);
+const Color lightGreen500 = Color(0xFF8BC34A);
+const Color lightGreen600 = Color(0xFF7CB342);
+const Color lightGreen700 = Color(0xFF689F38);
+const Color lightGreen800 = Color(0xFF558B2F);
+const Color lightGreen900 = Color(0xFF33691E);
+
+// Lime Shades
+const Color lime50 = Color(0xFFF9FBE7);
+const Color lime100 = Color(0xFFF0F4C3);
+const Color lime200 = Color(0xFFE6EE9C);
+const Color lime300 = Color(0xFFDCE775);
+const Color lime400 = Color(0xFFD4E157);
+const Color lime500 = Color(0xFFCDDC39);
+const Color lime600 = Color(0xFFC0CA33);
+const Color lime700 = Color(0xFFAFB42B);
+const Color lime800 = Color(0xFF9E9D24);
+const Color lime900 = Color(0xFF827717);
+
+// Yellow Shades
+const Color yellow50 = Color(0xFFFFFDE7);
+const Color yellow100 = Color(0xFFFFF9C4);
+const Color yellow200 = Color(0xFFFFF59D);
+const Color yellow300 = Color(0xFFFFF176);
+const Color yellow400 = Color(0xFFFFEE58);
+const Color yellow500 = Color(0xFFFFEB3B);
+const Color yellow600 = Color(0xFFFDD835);
+const Color yellow700 = Color(0xFFFBC02D);
+const Color yellow800 = Color(0xFFF9A825);
+const Color yellow900 = Color(0xFFF57F17);
+
+// Amber Shades
+const Color amber50 = Color(0xFFFFF8E1);
+const Color amber100 = Color(0xFFFFECB3);
+const Color amber200 = Color(0xFFFFE082);
+const Color amber300 = Color(0xFFFFD54F);
+const Color amber400 = Color(0xFFFFCA28);
+const Color amber500 = Color(0xFFFFC107);
+const Color amber600 = Color(0xFFFFB300);
+const Color amber700 = Color(0xFFFFA000);
+const Color amber800 = Color(0xFFFF8F00);
+const Color amber900 = Color(0xFFFF6F00);
+
+// Orange Shades
+const Color orange50 = Color(0xFFFFF3E0);
+const Color orange100 = Color(0xFFFFE0B2);
+const Color orange200 = Color(0xFFFFCC80);
+const Color orange300 = Color(0xFFFFB74D);
+const Color orange400 = Color(0xFFFFA726);
+const Color orange500 = Color(0xFFFF9800);
+const Color orange600 = Color(0xFFFB8C00);
+const Color orange700 = Color(0xFFF57C00);
+const Color orange800 = Color(0xFFEF6C00);
+const Color orange900 = Color(0xFFE65100);
+
+// Deep Orange Shades
+const Color deepOrange50 = Color(0xFFFBE9E7);
+const Color deepOrange100 = Color(0xFFFFCCBC);
+const Color deepOrange200 = Color(0xFFFFAB91);
+const Color deepOrange300 = Color(0xFFFF8A65);
+const Color deepOrange400 = Color(0xFFFF7043);
+const Color deepOrange500 = Color(0xFFFF5722);
+const Color deepOrange600 = Color(0xFFF4511E);
+const Color deepOrange700 = Color(0xFFE64A19);
+const Color deepOrange800 = Color(0xFFD84315);
+const Color deepOrange900 = Color(0xFFBF360C);
+
+// Brown Shades
+const Color brown50 = Color(0xFFEFEBE9);
+const Color brown100 = Color(0xFFD7CCC8);
+const Color brown200 = Color(0xFFBCAAA4);
+const Color brown300 = Color(0xFFA1887F);
+const Color brown400 = Color(0xFF8D6E63);
+const Color brown500 = Color(0xFF795548);
+const Color brown600 = Color(0xFF6D4C41);
+const Color brown700 = Color(0xFF5D4037);
+const Color brown800 = Color(0xFF4E342E);
+const Color brown900 = Color(0xFF3E2723);
+
+// White Shades
+const Color white50 = Color(0xFFFFFFFF);
+const Color white100 = Color(0xFFF9F9F9);
+const Color white200 = Color(0xFFF2F2F2);
+const Color white300 = Color(0xFFEAEAEA);
+const Color white400 = Color(0xFFDADADA);
+const Color white500 = Color(0xFFCCCCCC);
+const Color white600 = Color(0xFFB3B3B3);
+const Color white700 = Color(0xFF999999);
+const Color white800 = Color(0xFF808080);
+const Color white900 = Color(0xFF666666);
+
+
+// Grey Shades
+const Color grey50 = Color(0xFFFAFAFA);
+const Color grey100 = Color(0xFFF5F5F5);
+const Color grey200 = Color(0xFFEEEEEE);
+const Color grey300 = Color(0xFFE0E0E0);
+const Color grey400 = Color(0xFFBDBDBD);
+const Color grey500 = Color(0xFF9E9E9E);
+const Color grey600 = Color(0xFF757575);
+const Color grey700 = Color(0xFF616161);
+const Color grey800 = Color(0xFF424242);
+const Color grey900 = Color(0xFF212121);
+
+// BlueGrey Shades
+const Color blueGrey50 = Color(0xFFECEFF1);
+const Color blueGrey100 = Color(0xFFCFD8DC);
+const Color blueGrey200 = Color(0xFFB0BEC5);
+const Color blueGrey300 = Color(0xFF90A4AE);
+const Color blueGrey400 = Color(0xFF78909C);
+const Color blueGrey500 = Color(0xFF607D8B);
+const Color blueGrey600 = Color(0xFF546E7A);
+const Color blueGrey700 = Color(0xFF455A64);
+const Color blueGrey800 = Color(0xFF37474F);
+const Color blueGrey900 = Color(0xFF263238);
